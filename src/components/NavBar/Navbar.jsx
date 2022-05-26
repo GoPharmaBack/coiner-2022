4c162ff850e079cd5038a07dc7ca1a427b9ac105
Carlos Boyzo <carlos.boor@gmail.com>
2022-05-26 13:35:54 -0500

    nuevo sitio upload

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar () {
+  return (
+    <>
+
+      <nav>
+        <div className="nav-wrapper">
+
+          <ul>
+            <li className='btn-primary'><a href="https://2021.coiner.org/">Revive COINER<sup>®</sup> 2021</a></li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
